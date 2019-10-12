ad9baca2bd846db407936093fef827ed068d2577
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-12 23:00:42 +0300

    define a function that returns the searched query

--- insta/views.py
+++ insta/views.py
@@ -6,3 +6,16 @@ from .models import Image
 def home(request):
     posts=Image.get_imgs()  
     return render(request,'insta/home.html',{'posts':posts})
+
+def search_results(request):
+    # view function that fetches posts based on search terms
+    if 'post' in request.GET and request.GET["post"]:
+        search_term=request.GET.get('post')
+        search_posts=Image.search(search_term)
+        message=f"{search_term}"
+
+        return render(request,'insta/search.html',{'message':message,"posts":search_posts})
+
+    else:
+        message='You havent searched for any term'
+        return render(request,'insta/search.html',{'message':message}) 
