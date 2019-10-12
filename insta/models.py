d8ce16ab7b5ecbb18fc39b5f9f6c7f373ab7605b
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-12 21:03:01 +0300

    add the tags to bind similar photos

--- insta/models.py
+++ insta/models.py
@@ -8,6 +8,15 @@ class Image(models.Model):
     poster=models.ImageField(upload_to='posters/',default='')
     date_posted=models.DateTimeField(auto_now_add=True)
     last_modified=models.DateTimeField(default=timezone.now)
-
+    tags = models.ManyToManyField(tags)
+    
     def __str__(self):
         return f'Image{self.img_name}--{self.img_caption}'
+
+
+class tags(models.Model):
+    #tags to bind to photos
+    tag_name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tag_name
