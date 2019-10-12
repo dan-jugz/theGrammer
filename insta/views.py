9185b39346c49d643f68a4ae75e31f9975fb31b0
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-12 21:00:13 +0300

    define a function that renders the homepage

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+
 
 # Create your views here.
+def home(request):
+    posts=Image.get_imgs()  
+    return render(request,'insta/home.html',{'posts':posts})
