a176a6bd8d38eeeb27b8c1bf3680534a0dbc1a76
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-12 20:57:15 +0300

    setup an image model to define the image objects

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    img_name=models.CharField(max_length=100)
+    img_caption=HTMLField()
+    poster=models.ImageField(upload_to='posters/',default='')
+    date_posted=models.DateTimeField(auto_now_add=True)
+    last_modified=models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Image{self.img_name}--{self.img_caption}'
