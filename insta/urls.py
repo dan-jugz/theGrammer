f0ffb2186ad1f45d334cf5a7886712eaf867f018
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-13 03:15:02 +0300

    setup a route and display for search purposes

--- insta/urls.py
+++ insta/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('',views.home,name='instaHome'),
+    url(r'^',views.home,name='instaHome'),
+    url(r'^search/',views.search_results,name='search_results'),
 ]
