d59044211573ab54c9ada0d15fbd8fc5efff0688
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-13 07:09:35 +0300

    create the comment model

--- insta/models.py
+++ insta/models.py
@@ -17,6 +17,10 @@ class Image(models.Model):
     poster=models.ImageField(upload_to='posters/',default='')
     date_posted=models.DateTimeField(auto_now_add=True)
     last_modified=models.DateTimeField(default=timezone.now)
+    uploaded_by = models.ForeignKey(User,on_delete=models.CASCADE, default='')
+    comments = models.CharField(max_length =1000, default="No comments yet")
+    likes = models.IntegerField(default=0)
+    liked = models.BooleanField(default=False)
     tags = models.ManyToManyField(tags)
 
     def __str__(self):
@@ -51,3 +55,20 @@ class Image(models.Model):
     def delete_img(cls,img_id):
         # method to delete an image
         img=cls.objects.get(pk=img_id).delete()
+
+class Comments(models.Model):
+    image=models.ForeignKey(Image,on_delete=models.CASCADE),
+    comment_owner=models.ForeignKey(User,blank=True,null=True,on_delete=models.CASCADE)
+    comment_content=models.CharField(max_length=300,blank=True)
+
+    def save_comment(self):
+        self.save()
+
+    @classmethod
+    def delete_comment(cls,comment_id):
+        # method to delete a comment
+        comment=cls.objects.get(pk=comment_id)
+        comment.delete()    
+
+
+
