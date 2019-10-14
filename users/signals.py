778a1ee91cc634a7715dab56782879fe5aabd628
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-14 13:34:22 +0300

    define a function to fetch the signal and do the appropriate task

--- users/signals.py
+++ users/signals.py
@@ -9,4 +9,22 @@ from django.dispatch import receiver
 from .models import Profile
 
 
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    '''
+    post_save:is the signal that is fired after and objecte is saved
+
+    User:model is the sender of the signal
+
+    receiver:is the create profile function that fetches the signal and performs some task
+
+    instance:is the instance of User class
+
+    created : if user was created
+
+    Profile.objects.create(user=instance):create a profile obj with the instance of the user that was created
+
+    '''
+    if created:
+    Profile.objects.create(user=instance)
 
