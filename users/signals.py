74ef14440754aaaf5259b025a1caa172cc5c7ee9
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-14 13:33:19 +0300

    import sender and receiver for the signal

--- users/signals.py
+++ users/signals.py
@@ -1,3 +1,12 @@
 from django.db.models.signals import post_save
+
+# user will the sender of the signal
+from django.contrib.auth.models import User
+
+#import a receiver of the signal
+from django.dispatch import receiver
+
 from .models import Profile
+
+
 
