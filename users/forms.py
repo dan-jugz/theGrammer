683e22871168012717d28fb937026819d55e79d9
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-14 12:02:11 +0300

    update the username and the email of the user

--- users/forms.py
+++ users/forms.py
@@ -10,6 +10,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         # model to be affected
         model=User
-        
+
         # field i want in the form in order
         fields=['username','email','password1','password2']
+
+
+
+# update username and email of the user 
+class UserUpdateForm(forms.ModelForm):
+    email=forms.EmailField()
+
+    class Meta:
+        model=User
+        fields=['username','email']    
