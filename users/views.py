67e4f6acfa497a18975e5779347970332428e786
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-14 11:32:45 +0300

    define a profile function that  gets the posts that a user has and displays them  on their profike page

--- users/views.py
+++ users/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
+from .models import Profile
 
 # Create your views here
 # register user on the app
@@ -14,6 +22,36 @@ def register(request):
     else:      
         form=UserRegisterForm()
     return render(request,'user/register.html',{'form':form})
+
+
+
+
+@login_required
+def profile(request):
+    if request.method=='POST':
+        usrForm=UserUpdateForm(request.POST,instance=request.user)
+        profForm=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if usrForm.is_valid() and profForm.is_valid():
+            usrForm.save()
+            profForm.save()
+
+            messages.success(request,'Your account has been updated!')
+            return redirect('profile')
+    else:
+        usrForm=UserUpdateForm(instance=request.user)
+        profForm=ProfileUpdateForm(instance=request.user.profile) 
+
+
+    user=User.objects.get(pk=request.user.id)
+    posts=user.image_set.all()
+
+    context={
+        'usrForm':usrForm,
+        'profForm':profForm,
+        'posts':posts
+    }
+    return render(request,'user/profile.html',context)
+
 
 
 # view function to redirect to a user profile
