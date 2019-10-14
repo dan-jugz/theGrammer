fa7292434487a9233c3b614e6276e0c18fdbac37
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-14 11:18:01 +0300

    define a function that helps with regis
    tration and a template to use

--- users/views.py
+++ users/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here
+# register user on the app
+def register(request):
+    if request.method=='POST':
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request,f'Account Created for {username}')
+            return redirect('login')
+
+    else:      
+        form=UserRegisterForm()
+    return render(request,'user/register.html',{'form':form})
+
+
 # view function to redirect to a user profile
 def user(request,pk):    
     try:
