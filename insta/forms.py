8c6e0a302b45831065b2c1c8f4211d57ffa5b52c
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-14 08:07:57 +0300

    adding  a model for the form to adding tags to a post

--- insta/forms.py
+++ insta/forms.py
@@ -5,4 +5,10 @@ class NewInstaPost(forms.ModelForm):
     class Meta:
         model=Image
         exclude=['author','date_posted','last_modified','tags']
+   
 
+# adding tags
+class AddTagsToPost(forms.ModelForm):
+    class Meta:
+        model=tags
+        fields=['tag_name'] 
