076b03b8b8f2650bc8edf9f88091b4ba7875f66b
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-14 04:35:29 +0300

    define a function for putting up a new post and the template associated

--- insta/views.py
+++ insta/views.py
@@ -17,8 +17,31 @@ def home(request):
         return redirect('instaHome')
     else:
         form=NewComment()
-    posts=Image.get_imgs()  
+        posts=Image.get_imgs()  
     return render(request,'insta/home.html',{'posts':posts})
+
+
+@login_required
+def newInstaPost(request):
+    current_user=request.user
+    if request.method=='POST':
+        form=NewInstaPost(request.POST,request.FILES)
+        tagForm=AddTagsToPost(request.POST)
+        if form.is_valid() and tagForm.is_valid():
+            img_name=form.cleaned_data.get('img_name')
+            img=form.save(commit=False)
+            img.author=current_user
+            img.save()
+            tagForm.save()
+
+            messages.success(request,f'Post Created for {img_name}')
+        return redirect('instaHome')    
+    else:
+        form=NewInstaPost()
+        tagForm=AddTagsToPost()
+    return render(request,'insta/new_post.html',{'form':form,'tagForm':tagForm})
+
+
 
 def search_results(request):
     # view function that fetches posts based on search terms
