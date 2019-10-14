d4b1c025ed250d898b5954bd1a30f58d9ac1b081
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-14 11:04:30 +0300

    define a function that returns all a users posts and the template associated with it

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def user(request,pk):
+
+    posts=user.image_set.all()
+    return render(request,'user/user-profile.html',{'posts':posts,'user':user,'current_user':current_user})    
+
+
+
