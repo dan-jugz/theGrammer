9acca1c589d8279a02620fadce4ffe3383b80a34
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-14 11:09:22 +0300

    handle the error if the object does not exist

--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
-def user(request,pk):
+# Create your views here
+# view function to redirect to a user profile
+def user(request,pk):    
+    try:
+        user=User.objects.get(pk=pk)
+        current_user=request.user
+        
+    except ObjectDoesNotExist:
+        raise Http404
+        assert False
 
     posts=user.image_set.all()
     return render(request,'user/user-profile.html',{'posts':posts,'user':user,'current_user':current_user})    
