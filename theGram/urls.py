425091a62f7da3b21e652db4e5a5a248187e46fc
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-15 08:19:41 +0300

    configure routes and registration templates

--- theGram/urls.py
+++ theGram/urls.py
@@ -13,12 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path    
-
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
+from django.urls import path, re_path,include
+from users import views as user_views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/',user_views.register,name='register'),
+    path('profile/',user_views.profile,name='profile'),
+    path('user/<int:pk>/',user_views.user,name='user-profile'),
     path('',include('insta.urls')),
+    path('accounts/login',include('django.contrib.auth.urls')),
+    path('login/',auth_views.LoginView.as_view(template_name='user/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name='logout'),
+    url(r'^tinymce/',include('tinymce.urls')),
+    
 ]
+permanent=True
